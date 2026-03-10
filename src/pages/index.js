819a56f80d82bee55d5652259a4dd54f03ac63b3
yuryqwer <yuryqwer@outlook.com>
2026-03-10 21:54:22 +0800

    Make the homepage button jump to the right link

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="/docs/guide/foreword">
             开始阅读
           </Link>
         </div>
